8f8ad06ffdeff2d783cb67d1e16a03d5449ce470
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-05-18 21:25:34 -0700

    Added file

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,38 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src'),
     },
-    extensions: ['.js', '.scss', '.css'],
+    extensions: ['.ts', '.js', '.scss', '.css'],
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Test for .js files
-        exclude: /node_modules/, // Exclude node_modules
-        use: {
-          loader: 'babel-loader', // Use Babel to transpile ES6 to ES5
-          options: {
-            presets: ['@babel/preset-env'], // Babel preset for ES6+ features
-          },
-        },
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
     ],
   },
@@ -41,7 +47,7 @@ module.exports = {
     port: 9000,
     hot: true,
     static: './build',
-    open: true
+    open: true,
   },
-  mode: 'development'
+  mode: 'development',
 };
